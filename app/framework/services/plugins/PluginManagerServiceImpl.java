04b59f3c2ace796181f430ee8d28eee9fc1a0251
jkohlertaf <johann.kohler@the-agile-factory.com>
2016-01-27 10:20:36 +0100

    Decrease log level for notification (refs #TAF-273)

--- app/framework/services/plugins/PluginManagerServiceImpl.java
+++ app/framework/services/plugins/PluginManagerServiceImpl.java
@@ -831,8 +831,8 @@ public class PluginManagerServiceImpl implements IPluginManagerService, IEventBr
     }
 
     @Override
-    public Pair<IPluginConfigurationBlockDescriptor, byte[]> getPluginConfigurationBlock(Long pluginConfigurationId, String pluginConfigurationBlockIdentifier)
-            throws PluginException {
+    public Pair<IPluginConfigurationBlockDescriptor, byte[]> getPluginConfigurationBlock(Long pluginConfigurationId,
+            String pluginConfigurationBlockIdentifier) throws PluginException {
         PluginRegistrationEntry pluginRegistrationEntry = getPluginByIds().get(pluginConfigurationId);
         if (pluginRegistrationEntry == null) {
             throw new PluginException("Unknown plugin configuration :" + pluginConfigurationId);
@@ -965,7 +965,8 @@ public class PluginManagerServiceImpl implements IPluginManagerService, IEventBr
                             .getPluginConfigurationBlockFromIdentifier(pluginConfigurationId, xmlDesc.getIdentifier());
                     if (pluginConfigurationBlock == null) {
                         if (log.isDebugEnabled()) {
-                            log.debug(String.format("Creating a new config block %s plugin %d configuration", xmlDesc.getIdentifier(), pluginConfigurationId));
+                            log.debug(
+                                    String.format("Creating a new config block %s plugin %d configuration", xmlDesc.getIdentifier(), pluginConfigurationId));
                         }
                         pluginConfigurationBlock = new PluginConfigurationBlock();
                         pluginConfigurationBlock.configurationType = xmlDesc.getType();
@@ -1676,7 +1677,8 @@ public class PluginManagerServiceImpl implements IPluginManagerService, IEventBr
                         log.error(String.format("The plugin %d has reported an error while stopping, id of error is %s", pluginConfigurationId, uuid), e);
                         PluginLog.saveStopPluginLog(pluginConfigurationId, getMessagesPlugin().get("plugin.failed.stop", pluginConfigurationId, uuid), true);
                     }
-                    getPluginStatusCallbackActorRef().tell(new CallbackLifeCycleMessage(PluginStatus.STOPPED, getPluginConfigurationId()), ActorRef.noSender());
+                    getPluginStatusCallbackActorRef().tell(new CallbackLifeCycleMessage(PluginStatus.STOPPED, getPluginConfigurationId()),
+                            ActorRef.noSender());
                     break;
                 }
             } else {
@@ -1759,14 +1761,14 @@ public class PluginManagerServiceImpl implements IPluginManagerService, IEventBr
             if (message != null && message instanceof EventMessage) {
                 EventMessage eventMessage = (EventMessage) message;
                 try {
-                    log.info(String.format("[BEGIN] Transaction %s for event message processing actor for plugin %d with message type %s",
+                    log.debug(String.format("[BEGIN] Transaction %s for event message processing actor for plugin %d with message type %s",
                             eventMessage.getTransactionId(), getPluginConfigurationId(), eventMessage.getMessageType().name()));
                     if (getFlowType().equals(FlowType.OUT)) {
                         getPluginRunner().handleOutProvisioningMessage(eventMessage);
                     } else {
                         getPluginRunner().handleInProvisioningMessage(eventMessage);
                     }
-                    log.info(String.format("[SUCCESS] Transaction %s for event message processing actor for plugin %d with message type %s",
+                    log.debug(String.format("[SUCCESS] Transaction %s for event message processing actor for plugin %d with message type %s",
                             eventMessage.getTransactionId(), getPluginConfigurationId(), eventMessage.getMessageType().name()));
                 } catch (PluginException e) {
                     /*
@@ -1785,9 +1787,9 @@ public class PluginManagerServiceImpl implements IPluginManagerService, IEventBr
                     } else {
                         String errorMessage = String.format("[FAILURE] Transaction %s for event message processing actor for plugin %d failed",
                                 eventMessage.getTransactionId(), getPluginConfigurationId());
-                        PluginLog.saveOnEventHandlingPluginLog(eventMessage.getTransactionId(), getPluginConfigurationId(), true, eventMessage.getMessageType(),
-                                errorMessage + "\nMessage was : " + eventMessage.toString(), eventMessage.getDataType(), eventMessage.getInternalId(),
-                                eventMessage.getExternalId());
+                        PluginLog.saveOnEventHandlingPluginLog(eventMessage.getTransactionId(), getPluginConfigurationId(), true,
+                                eventMessage.getMessageType(), errorMessage + "\nMessage was : " + eventMessage.toString(), eventMessage.getDataType(),
+                                eventMessage.getInternalId(), eventMessage.getExternalId());
                         throw new PluginException(errorMessage, e);
                     }
                 }
