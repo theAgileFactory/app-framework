c619ffe0b9e5b4995fb7f79b50d07e6eaaed56fc
Marc Schaer <marc110191@gmail.com>
2016-03-31 12:17:35 +0200

    update token

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -4,9 +4,13 @@ set -e
 
 mkdir -p git/snapshots/com/agifac/lib/app-framework/$1
 
-cp ~/.m2/repository/com/agifac/lib/app-framework/$1/*.jar git/snapshots/com/agifac/lib/app-framework
-cp ~/.m2/repository/com/agifac/lib/app-framework/$1/*.zip git/snapshots/com/agifac/lib/app-framework
-cp ~/.m2/repository/com/agifac/lib/app-framework/$1/*.pom git/snapshots/com/agifac/lib/app-framework
+cp ~/.m2/repository/com/agifac/lib/app-framework/$1/*.jar git/snapshots/com/agifac/lib/app-framework/$1
+cp ~/.m2/repository/com/agifac/lib/app-framework/$1/*.zip git/snapshots/com/agifac/lib/app-framework/$1
+cp ~/.m2/repository/com/agifac/lib/app-framework/$1/*.pom git/snapshots/com/agifac/lib/app-framework/$1
+
+wget https://github.com/github/git-lfs/releases/download/v1.1.2/git-lfs-linux-amd64-1.1.2.tar.gz
+tar -xzf git-lfs-linux-amd64-1.1.2.tar.gz
+cd git-lfs-1.1.2 && ./install.sh
 
 cd git
 git init
@@ -14,7 +18,11 @@ git init
 git config user.name "Travis CI"
 git config user.email "marc.schar@the-agile-factory.com"
 
-git add .
+git lfs install
+git lfs track *.zip
+git lfs track *.jar
+
+git add snapshots
 git commit -m "Deploy to GitHub Pages"
 
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+git push --force "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
