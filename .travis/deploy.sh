dcd18300d524b10ebd3fa53b76fe8534230f0bf6
Pierre-Yves Cloux <me@pycloux.com>
2016-04-30 12:10:36 +0200

    Adding automatic deployment to Sonatype

---
+++ .travis/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [ ! -z "$TRAVIS_TAG" ]
+then
+    echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
+    mvn -f pom2.xml --settings ~/.m2/settings.xml versions:set -DnewVersion=$TRAVIS_TAG 1>/dev/null 2>/dev/null
+else
+    echo "not on a tag -> keep snapshot version in pom.xml"
+fi
+mvn clean deploy --settings ~/.m2/settings.xml -DskipTests=true -B -U
